7ebc46c1afddc555e172fae68804d93f216db6b1
Panda Kontrol <kevinkontrol97@gmail.com>
2022-10-18 19:03:12 +0200

    addToBox first function
    trying how to first function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hola')
+let box = document.getElementById('result');
+let numbers = document.querySelectorAll('input.number');
+numbers.addEventListener('click', addToBox)
+function addToBox() {
+    
+    
+    for (let i = 0; i < numbers.length; i++) {
+        numbers['i'].value
+            
+    }
+}
